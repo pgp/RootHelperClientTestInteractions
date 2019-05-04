16798bf4a2c43c489a00b0d9e182f3b45e65ec9e
pgp <pgp@gmx.it>
2019-05-04 13:53:35 +0200

    aligned HTTPS download client (ref.: Roothelper 1.0.5_20190504)

--- tls/https_download_url.py
+++ tls/https_download_url.py
@@ -21,14 +21,19 @@ def remote_client_url_download(serverHost="v.gd",serverPort=443, downloadPath='/
 	bDestPath = bytearray(downloadPath.encode("utf-8"))
 	sock.sendall(struct.pack("@H", len(bDestPath)))
 	sock.sendall(bDestPath)
-
-	resp = sock.recv(1)  # response first byte: \x00 OK or \xFF ERROR
-	if resp != b'\x00':
-		print("Error byte received, errno is:", struct.unpack("@I", sock.recv(4))[0])
-		return
-
-	# receive TLS shared session secret hash (64-byte hex encoded SHA256)
-	print("TLS master secret hash is:", toHex(sock.recv(64)))
+	
+	# Server may perform more than one TLS handshake (due to 301/302 HTTP redirects), so send explicit EOF before sending download size
+	while True:
+		resp = sock.recv(1)  # response first byte: \x00 OK or \xFF ERROR
+		if resp != b'\x00':
+			if resp == b'\x11':
+				print('End of redirects')
+				break
+			print("Error byte received, errno is:", struct.unpack("@I", sock.recv(4))[0])
+			return
+		# receive TLS shared session secret hash (64-byte hex encoded SHA256)
+		print("TLS master secret hash is:", toHex(sock.recv(64)))
+	
 	print("Download size is:", struct.unpack("@Q", sock.recv(8))[0])
 
 	while True:
@@ -42,12 +47,12 @@ def remote_client_url_download(serverHost="v.gd",serverPort=443, downloadPath='/
 
 
 if __name__ == '__main__':
-	# rclient = remote_client_url_download(serverHost="v.gd",serverPort=443,downloadPath='/dev/shm/xfileshttpclient.txt') # mandatory SNI
+	# rclient = remote_client_url_download(serverHost="v.gd",serverPort=443,downloadPath='/dev/shm/vgd.html') # mandatory SNI
 	# rclient = remote_client_url_download(serverHost="fancyssl.hboeck.de",serverPort=443,downloadPath='/dev/shm/urldownload.txt') # TLS 1.2 only
 	# rclient = remote_client_url_download(serverHost="download-installer.cdn.mozilla.net/pub/firefox/releases/66.0.3/win64/it/Firefox%20Setup%2066.0.3.exe",serverPort=443,downloadPath='/dev/shm/ff.exe')
-	# rclient = remote_client_url_download(serverHost="u.nu",serverPort=443,downloadPath='/dev/shm/unu.txt')
-	rclient = remote_client_url_download(serverHost="cloudflare.com",serverPort=443) # HTTP 301
-	# rclient = remote_client_url_download(serverHost="www.cloudflare.com",serverPort=443,downloadPath='/dev/shm/cloudflare.txt')
+	# rclient = remote_client_url_download(serverHost="u.nu",serverPort=443,downloadPath='/dev/shm/unu.html')
+	rclient = remote_client_url_download(serverHost="cloudflare.com",serverPort=443,downloadPath='/dev/shm/cloudflare.html') # HTTP 301
+	# rclient = remote_client_url_download(serverHost="www.cloudflare.com",serverPort=443,downloadPath='/dev/shm/www_cloudflare.html')
 	if rclient is None:
 		sys.exit(-1)
 	rclient.close()
