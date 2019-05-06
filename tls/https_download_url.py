5ab804aecae2fe4bca3044a1a601b82fc4a23ee3
PGP <pgp@gmx.it>
2019-05-06 13:53:39 +0200

    added targetFilename paramater in https url downloader

--- tls/https_download_url.py
+++ tls/https_download_url.py
@@ -1,58 +1,67 @@
 import struct
 import sys
+
 if not '..' in sys.path: sys.path.append('..')
 from net_common import *
 import sys
 
 
-def remote_client_url_download(serverHost="v.gd",serverPort=443, downloadPath='/dev/shm/ciao.bin', unixSocketNameWithoutTrailingNull='anotherRoothelper'):
-	sock = get_connected_local_socket(unixSocketNameWithoutTrailingNull)
+def remote_client_url_download(serverHost="v.gd",
+                               serverPort=443,
+                               downloadDir='/dev/shm',
+                               targetFilename='',
+                               unixSocketNameWithoutTrailingNull='anotherRoothelper'):
+    sock = get_connected_local_socket(unixSocketNameWithoutTrailingNull)
 
-	rq = bytearray(b'\x18') # ACTION_HTTPS_URL_DOWNLOAD request
-	sock.sendall(rq)
+    rq = bytearray(b'\x18')  # ACTION_HTTPS_URL_DOWNLOAD request
+    sock.sendall(rq)
 
-	# send string-with-length of IP
-	bServerHost = bytearray(serverHost.encode("utf-8"))
-	sock.sendall(struct.pack("@H",len(bServerHost)))
-	sock.sendall(bServerHost)
-	# send port
-	sock.sendall(struct.pack("@H",serverPort))
-	# send download destination path
-	bDestPath = bytearray(downloadPath.encode("utf-8"))
-	sock.sendall(struct.pack("@H", len(bDestPath)))
-	sock.sendall(bDestPath)
-	
-	# Server may perform more than one TLS handshake (due to 301/302 HTTP redirects), so send explicit EOF before sending download size
-	while True:
-		resp = sock.recv(1)  # response first byte: \x00 OK or \xFF ERROR
-		if resp != b'\x00':
-			if resp == b'\x11':
-				print('End of redirects')
-				break
-			print("Error byte received, errno is:", struct.unpack("@I", sock.recv(4))[0])
-			return
-		# receive TLS shared session secret hash (64-byte hex encoded SHA256)
-		print("TLS master secret hash is:", toHex(sock.recv(64)))
-	
-	print("Download size is:", struct.unpack("@Q", sock.recv(8))[0])
+    # send string-with-length of IP
+    bServerHost = bytearray(serverHost.encode("utf-8"))
+    sock.sendall(struct.pack("@H", len(bServerHost)))
+    sock.sendall(bServerHost)
+    # send port
+    sock.sendall(struct.pack("@H", serverPort))
+    # send download destination path
+    bDestPath = bytearray(downloadDir.encode("utf-8"))
+    sock.sendall(struct.pack("@H", len(bDestPath)))
+    sock.sendall(bDestPath)
+    # send target filename
+    bTargetFilename = bytearray(targetFilename.encode("utf-8"))
+    sock.sendall(struct.pack("@H", len(bTargetFilename)))
+    if bTargetFilename: sock.sendall(bTargetFilename)
 
-	while True:
-		p = struct.unpack("@Q", sock.recv(8))[0]
-		if p == 2**64-1:
-			break
-		print("Progress:",p)
-	print("Completed")
+    # Server may perform more than one TLS handshake (due to 301/302 HTTP redirects), so send explicit EOF before sending download size
+    while True:
+        resp = sock.recv(1)  # response first byte: \x00 OK or \xFF ERROR
+        if resp != b'\x00':
+            if resp == b'\x11':
+                print('End of redirects')
+                break
+            print("Error byte received, errno is:", struct.unpack("@I", sock.recv(4))[0])
+            return
+        # receive TLS shared session secret hash (64-byte hex encoded SHA256)
+        print("TLS master secret hash is:", toHex(sock.recv(64)))
 
-	return sock
+    print("Download size is:", struct.unpack("@Q", sock.recv(8))[0])
+
+    while True:
+        p = struct.unpack("@Q", sock.recv(8))[0]
+        if p == 2 ** 64 - 1:
+            break
+        print("Progress:", p)
+    print("Completed")
+
+    return sock
 
 
 if __name__ == '__main__':
-	# rclient = remote_client_url_download(serverHost="v.gd",serverPort=443,downloadPath='/dev/shm/vgd.html') # mandatory SNI
-	# rclient = remote_client_url_download(serverHost="fancyssl.hboeck.de",serverPort=443,downloadPath='/dev/shm/urldownload.txt') # TLS 1.2 only
-	# rclient = remote_client_url_download(serverHost="download-installer.cdn.mozilla.net/pub/firefox/releases/66.0.3/win64/it/Firefox%20Setup%2066.0.3.exe",serverPort=443,downloadPath='/dev/shm/ff.exe')
-	# rclient = remote_client_url_download(serverHost="u.nu",serverPort=443,downloadPath='/dev/shm/unu.html')
-	rclient = remote_client_url_download(serverHost="cloudflare.com",serverPort=443,downloadPath='/dev/shm/cloudflare.html') # HTTP 301
-	# rclient = remote_client_url_download(serverHost="www.cloudflare.com",serverPort=443,downloadPath='/dev/shm/www_cloudflare.html')
-	if rclient is None:
-		sys.exit(-1)
-	rclient.close()
+    # rclient = remote_client_url_download(serverHost="v.gd") # mandatory SNI
+    # rclient = remote_client_url_download(serverHost="fancyssl.hboeck.de",serverPort=443) # TLS 1.2 only
+    # rclient = remote_client_url_download(serverHost="download-installer.cdn.mozilla.net/pub/firefox/releases/66.0.3/win64/it/Firefox%20Setup%2066.0.3.exe", targetFilename='ff.exe')
+    # rclient = remote_client_url_download(serverHost="u.nu",serverPort=443,targetFilename='unu.html')
+    rclient = remote_client_url_download(serverHost="cloudflare.com",targetFilename="cloudflare.html") # HTTP 301
+    # rclient = remote_client_url_download(serverHost="www.cloudflare.com",serverPort=443,targetFilename='www_cloudflare.html')
+    if rclient is None:
+        sys.exit(-1)
+    rclient.close()
