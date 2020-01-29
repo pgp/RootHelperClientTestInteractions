0be1a923ff1079b72b99b7b9aed72dc294664d13
pgp <pgp@gmx.it>
2020-01-29 15:19:44 +0100

    updated protocol (dir hash, RH v1.4.3)

--- checksums.py
+++ checksums.py
@@ -2,15 +2,24 @@ from net_common import *
 import struct
 import sys
 
+def getDirHashOpts(withNames=False,
+                   ignoreThumbsFiles=True,
+                   ignoreUnixHiddenFiles=True,
+                   ignoreEmptyDirs=True):
+    return bytearray([((1 if withNames else 0) +
+                       (2 if ignoreThumbsFiles else 0) +
+                       (4 if ignoreUnixHiddenFiles else 0) +
+                       (8 if ignoreEmptyDirs else 0))])
 
 if __name__ == "__main__":
     sock = get_connected_local_socket()
 
-    path = encodeString('/sdcard/1.7z')
+    path = encodeString('/dev/shm/exampleDir')
     # path = encodeString('/dev/null')
     sock.sendall(bytearray(b'\x0A'))  # HASH request
     # sock.sendall(bytearray(b'\x01'))  # choose MD5 algorithm
     sock.sendall(bytearray(b'\x06'))  # choose SHA3-224 algorithm
+    sock.sendall(getDirHashOpts(withNames=True,ignoreUnixHiddenFiles=False))  # send dirHashOpts byte (unused for regular files)
     sock.sendall(struct.pack("@H", len(path)))  # len of path as unsigned short
     sock.sendall(path)
 
