7b1d8e3dbb47df93719abddf67d0fda240ed1700
pgp <pgp@gmx.it>
2023-02-11 11:13:12 +0100

    keep into account two flag bits in creation strategy byte when creating random files in create_file_or_dir.py: for supplying custom seed for PRNG init, and for requesting hash output once file generation is complete (reference RH version: 1.9.5_20230211)

--- create_file_or_dir.py
+++ create_file_or_dir.py
@@ -1,9 +1,9 @@
 from net_common import *
-from standalone_xre.xre_common import sendStringWithLen
+from standalone_xre.xre_common import sendStringWithLen, receiveStringWithLen
 import struct
 
 CREATE_FILE_RQ_BYTE = bytearray([ord(b'\x09') ^ (1 << 5)])
-CREATE_FILE_ADV_RQ_BYTE = bytearray([ord(b'\x09') ^ (3 << 5)]) # create file with advanced options (flags 110)
+CREATE_FILE_ADV_RQ_BYTE = bytearray([ord(b'\x09') ^ (3 << 5)]) # create file with advanced options (flags 011)
 CREATE_DIR_RQ_BYTE = bytearray([ord(b'\x09') ^ (0 << 5)])
 
 # server giving permission denied if run from within CLion
@@ -25,16 +25,31 @@ if __name__ == "__main__":
     sock.sendall(file_path)
     sock.sendall(fmode)
 
-    # creation strategy: 0 fallocate, 1 zeros, 2 random, 3 random with custom seed (deterministic output)
-    #### sock.sendall(b'\x00') # fallocate # not supported by Android API 19, disabled
-    # sock.sendall(b'\x01') # zeros
-    # sock.sendall(b'\x02') # random
-    sock.sendall(b'\x03') # random with custom seed
-    # send seed (only if strategy == 3)
-    sendStringWithLen(sock, 'customSeed1234')
+    # creation strategy: 0 fallocate (not supported by Android API 19, disabled), 1 zeros, 2 random, plus two flag bits
 
-    sock.sendall(struct.pack("=Q", 2**30)) # 1 Gb file
+    # creationStrategy = 0 # b'\x00' # fallocate
+    # creationStrategy = 1 # b'\x01' # zeros
+    creationStrategy = 2   # b'\x02' # random
+
+    creationStrategy |= 4  # supply custom seed for initializing PRNG
+    creationStrategy |= 8  # request hash output for generated file
+
+    sock.sendall(creationStrategy.to_bytes(1)) # byte order is not necessary here, since we are only packing a single byte
+
+    sock.sendall(struct.pack("=Q", 2 ** 30))  # send size - 1 GB file
     # sock.sendall(struct.pack("=Q", 0)) # 0 b (empty) file
+
+    # send seed if the corresponding flag is enabled
+    if creationStrategy & 2:
+        if creationStrategy & 4:
+            prngSeed = 'customSeed1234'
+            print(f'Supplying custom seed for PRNG init: {prngSeed}')
+            sendStringWithLen(sock, prngSeed)
+        # send hash type for output hash, if the corresponding flag is enabled
+        if creationStrategy & 8:
+            targetHash = 'sha224'
+            print(f'Requesting output hash {targetHash} for generated file')
+            sendStringWithLen(sock, targetHash)
 
     resp = sock.recv(1)  # response first byte: \x00 OK or \xFF ERROR
 
@@ -49,4 +64,7 @@ if __name__ == "__main__":
                 if currentProgress == 2 ** 64 - 1:
                     print("End of progress")
                     break
+            if creationStrategy & 2 and creationStrategy & 8:  # (& 2) && (& 8), since the two flag bits are ignored if not creating a random file
+                output_hash = receiveStringWithLen(sock).decode('utf-8')
+                print(f'{targetHash} for the generated file is: {output_hash}')
     sock.close()
